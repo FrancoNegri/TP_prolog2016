838e440ef25dd9adbec0a9a585b6e8c3b0f56e84
FranckN <franconegri2004@hotmail.com>
2016-06-03 12:50:44 -0300

    Agregadas reversibilidad y test para funciones 4 y 7, modificada función 4 y 10

--- esqueleto.pl
+++ esqueleto.pl
@@ -57,10 +57,39 @@ palabras(S, [S]) :- not(member(espacio, S)).
 
 
 % Ejercicio 4 %
-asignar_var(Atomo, MI, [(Atomo, _) | MI]) :- not((member((Atomo, _), MI))).
+asignar_var(Atomo, MI, [(Atomo, _) | MI]) :- not((member((Atomo, _), MI))),!.
 asignar_var(Atomo, MI, MI) :- member((Atomo, _), MI).
 
+%testeo
+testsEj4:- testAs_v1, testAs_v2, testAs_v3.
+testAs_v1:- asignar_var(cuadrado, [], [(cuadrado, _G4012)]).
+testAs_v2:- asignar_var(cuadrado, [(rombo, _G4012)], [(cuadrado, _G4013),(rombo, _G4012)]).
+testAs_v3:- asignar_var(rombo, [(cuadrado, _G4013),(rombo, _G4012)], [ (cuadrado, _G4013), (rombo, _G4012)]).
 
+%reversibilidad:
+%Primer argumento:
+%asignar_var(A, [], [(cuadrado, _G4012)]).
+%asignar_var(A, [(rombo, _G4012)], [(cuadrado, _G4013),(rombo, _G4012)]).
+%asignar_var(A, [(cuadrado, _G4013),(rombo, _G4012)], [ (cuadrado, _G4013), (rombo, _G4012)]).
+%La función sin el primer valor instanciado devolverá el valor esperado
+
+%asignar_var(cuadrado, A, [(cuadrado, _G4012)]).
+%asignar_var(cuadrado, A, [(cuadrado, _G4013),(rombo, _G4012)]).
+%devuelve la lista sin el elemento agregado
+
+%asignar_var(A, B, [(cuadrado, _G4012)]).
+%A = cuadrado,
+%B = [].
+
+%asignar_var(A,B,C).
+%B = C, C = [ (A, _G24)|_G27] ;
+%B = C, C = [_G26, (A, _G24)|_G30] ;
+%B = C, C = [_G26, _G29, (A, _G24)|_G33] ;
+%B = C, C = [_G26, _G29, _G32, (A, _G24)|_G36] ;
+%...
+%este caso es medio turbio, B no tendría que asociar con C...
+
+%asignar_var(?A,?B,?C)
 
 % Ejercicio 5 %
 palabras_con_variables(S, V) :- diccionario_var(S, D), palabras_con_variables_y_dicc(S, D, V).
@@ -87,11 +116,33 @@ quitar(E, [X|XS], [X|R] ) :- not(X == E), quitar(E, XS, R).
 
 
 % Ejercicio 7 %
-% cant_distintos(L, S) %
-
+% cant_distintos(?L, ?S) %
 cant_distintos([], 0).
 cant_distintos([X|XS], N) :- quitar(X, XS, XSsinX), cant_distintos(XSsinX, N2), N is N2+1.
 
+%testeo
+testsEj7:-testCD1,testCD2.
+testCD1:-cant_distintos([A,B,A], 2).
+testCD2:-cant_distintos([], 0).
+
+%reversibilidad:
+%cant_distintos(A, 2).
+%A = [_G267, _G270].
+%luego cuelga buscando mas soluciones
+%cant_distintos(A, 0).
+%A =[].
+%luego cuelga buscando mas soluciones
+%cant_distintos(A, B).
+%A = [],
+%B = 0 ;
+%A = [_G282],
+%B = 1 ;
+%A = [_G282, _G285],
+%B = 2 ;
+%A = [_G282, _G285, _G297],
+%B = 3 ;
+%...
+%luego, cant_distintos(A,B) es reversible exceptuando el caso A no instanciado y B si.
 
 
 % Ejercicio 8 %
@@ -107,7 +158,7 @@ todos_distintos(XS) :- sacar_repe(XS, XS).
 
 to_string([], S) :- string_codes(S, []).
 to_string([XS], S) :- string_codes(S, XS).
-to_string([XS|XSS], S) :- length(XSS, N), N > 0, string_codes(S1, XS), to_string(XSS, S2), string_concat(" ", S2, S3), string_concat(S1, S3, S).
+to_string([XS|XSS], S) :- length(XSS, N), N > 0, string_codes(S1, XS), to_string(XSS, S2), string_concat(' ', S2, S3), string_concat(S1, S3, S).
 
 
 % Ejercicio 9 %
@@ -121,7 +172,7 @@ meter_espacios([X|XS], YS) :- meter_espacios(XS, REC), append([X], REC, YS).
 % Ejercicio 10 %
 mensajes_mas_parejos(S, M) :- descifrar_sin_espacios(S, M), dev_std(M, DSM), not((descifrar_sin_espacios(S, Y), dev_std(Y, DSY), DSM > DSY)).
 
-dev_std(M, DV) :- split_string(M, " ", "", L), stddev(L, DV).
+dev_std(M, DV) :- juntar_con(L, ' ', M), stddev(L, DV).
 
 stddev(List, STDdev) :- variance(List, Var), STDdev is sqrt(Var).
 
@@ -141,3 +192,9 @@ longitudes([X|Xs],[Y|Ys]) :- longitudes(Xs,Ys), string_length(X, Y).
 
 squares([],[]).
 squares([X|Xs],[Y|Ys]) :- squares(Xs,Ys), Y is X*X.
+
+%testeo
+testsEj10:-testMMP1, testMMP2.
+testMMP1:- cargar("dicc1.txt"),mensajes_mas_parejos([rombo, cuadrado, perro, cuadrado, sol, luna, triangulo, estrella, arbol, gato], A), A = 'casa de flor'.
+testMMP2:- cargar("dicc1.txt"),mensajes_mas_parejos([rombo, cuadrado, perro, cuadrado, sol, luna, triangulo, estrella, arbol, gato], "casa flor de").
+%No le gusta!!
